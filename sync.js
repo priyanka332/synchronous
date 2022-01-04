629dc1b0fa20c19953ee63f5b14c7e7c94850812
Priyanka Kumari <priyanka20@navgurukul.org>
2022-01-04 17:18:43 +0530

    Initial commit

---
+++ sync.js
@@ -0,0 +1,82 @@
+//  1st questions
+
+// function f1() {
+//     console.log("Bangalore Campus");
+// }
+
+// function f2() {
+//     console.log("Sarjapura Campus");
+// }
+
+// function f3() {
+//     console.log("Dharamshala Campus");
+// }
+
+// f1()
+// f2()
+// f3()
+
+
+
+
+
+//  2nd questions
+
+// var list = [3,5,4,6,8,1,10];
+// function array() {
+//     var arr = [];
+//     for (var i=0;i<list.length;i++) {
+//         if (list[i] % 2 == 0) {
+//             arr.push(list[i]);
+//         }
+//     }
+//     console.log(arr);
+// }
+
+// array()
+
+
+
+
+
+
+//  3rd questions
+
+// function count() {
+//     for (var i=1;i<=10;i++) {
+//         console.log(i);
+//     }
+// }
+// count()
+
+
+
+
+
+//  4th questions
+
+// var list = [3,5,4,6,8,1,10];
+// function array() {
+//     var arr = [];
+//     for (var i=0;i<list.length;i++) {
+//         if (list[i] % 2 != 0) {
+//             arr.push(list[i]);
+//         }
+//     }
+//     console.log(arr);
+// }
+
+// array()
+
+
+
+
+
+
+//  5th questions
+
+// function a() {
+//     console.log("Hi");
+//     console.log("I am Priyanka.");
+// }
+// a()
